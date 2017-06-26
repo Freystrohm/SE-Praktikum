8e1f1759f0d2f1e97635701e4d77239357cbdc6d
Luis Nickel <Luis Nickel@hotmail.de>
2017-06-26 20:12:07 +0200

    FestoProcessAccess

---
+++ Praktikum_3_4_Studierende/Sources_orig/FestoProcessAccessSim.cpp
@@ -0,0 +1,239 @@
+/**
+ * File:   FestoProcessAccess.cpp
+ *
+ * Class providing high level access to sensors and actuators of the FESTO
+ * system.
+ *
+ * \author  Prof. Dr. Thomas Lehmann
+ * \version 1
+ * \date    2013-11-01
+ */
+
+#include "iomasks.h"
+#include "FestoProcessAccessSim.h"
+#include <time.h>
+
+FestoProcessAccessSim::FestoProcessAccessSim() {
+    timeCounter = 0;
+
+
+#ifdef LOG_PROCESS
+    logFile = fopen("c:\\tmp\\processlog.txt","w");
+#else
+    logFile = NULL;
+#endif
+
+}
+
+FestoProcessAccessSim::~FestoProcessAccessSim() {
+    delete process;
+    if(logFile!=NULL){
+        fclose(logFile);
+    }
+}
+
+void FestoProcessAccessSim::updateInputs(void) {
+
+}
+void FestoProcessAccessSim::logProcessData(){
+
+}
+
+void FestoProcessAccessSim::applyOutput(void) {
+}
+
+void FestoProcessAccessSim::driveRight(void) {
+    directionRight=true;
+    directionLeft=false;
+    directionSlowLeft=false;
+    directionSlowRight=false;
+    directionStop=false;
+	printf("Drive Right");
+}
+
+void FestoProcessAccessSim::driveLeft(void) {
+	directionRight=false;
+	directionLeft=true;
+	directionSlowLeft=false;
+	directionSlowRight=false;
+	directionStop=false;
+	 printf("Drive Left");
+}
+
+void FestoProcessAccessSim::driveSlowRight(void) {
+	directionRight=false;
+	directionLeft=false;
+	directionSlowLeft=false;
+	directionSlowRight=true;
+	directionStop=false;
+	printf("Drives slow Right");
+}
+
+void FestoProcessAccessSim::driveSlowLeft(void) {
+	directionRight=true;
+	directionLeft=false;
+	directionSlowLeft=true;
+	directionSlowRight=false;
+	directionStop=false;
+	printf("Drives slow Left");
+}
+
+void FestoProcessAccessSim::driveStop(void) {
+	directionRight=false;
+	directionLeft=false;
+	directionSlowLeft=false;
+	directionSlowRight=false;
+	directionStop=true;
+	printf("Drive Stop");
+}
+
+void FestoProcessAccessSim::openJunction(void) {
+	junction=true;
+	printf("Open Junction");
+}
+
+void FestoProcessAccessSim::closeJunction(void) {
+	junction=false
+	printf("Close Junction");
+}
+
+void FestoProcessAccessSim::lightGreenOn(void) {
+	lightGreen=true;
+	printf("lightGreenON");
+}
+
+void FestoProcessAccessSim::lightGreenOff(void) {
+	lightGreen=false;
+	printf("lightGreenOff");
+}
+
+void FestoProcessAccessSim::lightYellowOn(void) {
+	lightYellow=true;
+	printf("lightYellowOn");
+}
+
+void FestoProcessAccessSim::lightYellowOff(void) {
+	lightYellow=false;
+	printf("lightYellowOff");
+}
+
+void FestoProcessAccessSim::lightRedOn(void) {
+	lightRed=true;
+	printf("lightRedOn");
+}
+
+void FestoProcessAccessSim::lightRedOff(void) {
+	lightRed=false
+	printf("lightRedOff");
+}
+
+void FestoProcessAccessSim::turnLEDStartOn(void) {
+	startLED=true;
+	printf("turnLEDStartOn");
+}
+
+void FestoProcessAccessSim::turnLEDStartOff(void) {
+	startLED=false;
+	printf("turnLEDStartOff");
+}
+
+void FestoProcessAccessSim::turnLEDResetOn(void) {
+	resetLED=true;
+	printf("turnLEDResetOn");
+};
+
+void FestoProcessAccessSim::turnLEDResetOff(void) {
+	resetLED=false;
+	printf("turnLEDResetOff");
+}
+
+void FestoProcessAccessSim::turnLEDQ1On(void) {
+	q1LED=true;
+	printf("turnLEDQ10nOn");
+}
+
+void FestoProcessAccessSim::turnLEDQ1Off(void) {
+	q1LED=false;
+	printf("turnLEDQ10nOff");
+}
+
+void FestoProcessAccessSim::turnLEDQ2On(void) {
+	q2LED=true;
+	printf("turnLEDQ20nOn");
+}
+
+void FestoProcessAccessSim::turnLEDQ2Off(void) {
+	q2LED=false;
+	printf("turnLEDQ20nOff");
+}
+
+void FestoProcessAccessSim::turnAllOff(void) {
+	lightGreen=false;
+	lightRed=false;
+	lightYellow=false;
+	q1LED=false;
+	q2LED=false;
+	printf("turnAllOff");
+}
+
+bool FestoProcessAccessSim::isItemAtBeginning(void) {
+    return beginningSensor; // active low
+}
+
+bool FestoProcessAccessSim::isItemAtHightSensor(void) {
+    return hightSensor; // active low
+};
+
+bool FestoProcessAccessSim::isItemAtMetalDetector(void) {
+    return metalDetector; // active low
+}
+
+bool FestoProcessAccessSim::isMetalDetected(void) {
+    return metalDetected;
+}
+
+bool FestoProcessAccessSim::isJunctionOpen(void) {
+    return junction;
+}
+
+bool FestoProcessAccessSim::isItemAtSlide(void) {
+    return slide; // active low
+}
+bool FestoProcessAccessSim::hasItemPassedSlide(void) {
+    return slidePassed; // active low, so pos edge
+}
+
+bool FestoProcessAccessSim::isItemAtEnd(void) {
+    return end; // active low
+}
+
+bool FestoProcessAccessSim::isButtonStartPressed(void) {
+    return buttonStartPressed;
+}
+
+bool FestoProcessAccessSim::isButtonStartPositiveEdge(void) {
+    return buttonStartPositiveEdge;
+}
+
+bool FestoProcessAccessSim::isButtonStartNegativeEdge(void) {
+    return buttonStartNegativeEdge;
+}
+
+bool FestoProcessAccessSim::isButtonStopPressed(void) {
+    return buttonStopPressed; // active low
+}
+
+bool FestoProcessAccessSim::isButtonResetPressed(void) {
+    return buttonResetPressed;
+}
+
+bool FestoProcessAccessSim::isButtonEmergencyActive(void) {
+    return buttonEmergencyActive; // active low
+}
+unsigned short FestoProcessAccessSim::getHight(){
+    return hight;
+}
+
+time_t FestoProcessAccessSim::timeCounter1s(){
+    return time(NULL);
+}
---
+++ Praktikum_3_4_Studierende/Sources_orig/FestoProcessAccessSim.h
@@ -0,0 +1,94 @@
+/*
+ * FestoProcessAccessSim.h
+ *
+ *  Created on: 26.06.2017
+ *      Author: Luis Nickel
+ */
+
+#ifndef SOURCES_ORIG_FESTOPROCESSACCESSSIM_H_
+#define SOURCES_ORIG_FESTOPROCESSACCESSSIM_H_
+
+#include "FestoProcessAccess.h"
+class FestoProcessAccessSim:public FestoProcessAccess {
+
+private:
+	bool directionRight;
+	bool directionLeft;
+	bool directionSlowRight;
+	bool directionSlowLeft;
+	bool directionStop;
+	bool junction;
+	bool lightGreen;
+	bool lightRed;
+	bool lightYellow;
+	bool startLED;
+	bool resetLED;
+	bool q1LED;
+	bool q2LED;
+	bool beginningSensor;
+	bool hightSensor;
+	bool metalDetector;
+	bool metalDetected;
+	bool slide;
+	bool slidePassed;
+	bool end;
+	bool buttonStartPressed;
+	bool buttonStartPositiveEdge;
+	bool buttonStartNegativeEdge;
+	bool buttonStopPressed;
+	bool buttonResetPressed;
+	bool buttonEmergencyActive;
+	unsigned short hight;
+
+
+
+public:
+    void updateInputs(void);
+    void applyOutput(void);
+public:
+    virtual void driveRight(void);
+    virtual void driveLeft(void);
+    virtual void driveSlowRight(void);
+    virtual void driveSlowLeft(void);
+    virtual void driveStop(void);
+    virtual void openJunction(void);
+    virtual void closeJunction(void);
+    virtual void lightGreenOn(void);
+    virtual void lightGreenOff(void);
+    virtual void lightYellowOn(void);
+    virtual void lightYellowOff(void);
+    virtual void lightRedOn(void);
+    virtual void lightRedOff(void);
+    virtual void turnLEDStartOn(void);
+    virtual void turnLEDStartOff(void);
+    virtual void turnLEDResetOn(void);
+    virtual void turnLEDResetOff(void);
+    virtual void turnLEDQ1On(void);
+    virtual void turnLEDQ1Off(void);
+    virtual void turnLEDQ2On(void);
+    virtual void turnLEDQ2Off(void);
+    virtual void turnAllOff(void);
+
+    virtual bool isItemAtBeginning(void);
+    virtual bool isItemAtHightSensor(void);
+    virtual bool isItemAtMetalDetector(void);
+    virtual bool isMetalDetected(void);
+    virtual bool isJunctionOpen(void);
+    virtual bool isItemAtSlide(void);
+    virtual bool hasItemPassedSlide(void);
+    virtual bool isItemAtEnd(void);
+    virtual bool isButtonStartPressed(void);
+    virtual bool isButtonStartPositiveEdge(void);
+    virtual bool isButtonStartNegativeEdge(void);
+    virtual bool isButtonStopPressed(void);
+    virtual bool isButtonResetPressed(void);
+    virtual bool isButtonEmergencyActive(void);
+    virtual unsigned short getHight();
+
+    virtual time_t timeCounter1s();
+private:
+    void logProcessData();
+};
+
+
+#endif /* SOURCES_ORIG_FESTOPROCESSACCESSSIM_H_ */
