ba4bb1801c108fbd0583fd748ba61dcfa90121d5
Johannes <Johannes@Jojo-Desktop>
2017-06-26 18:50:13 +0200

    Implement Hight FSM

--- Praktikum_3_4_Studierende/Sources_orig/fsm.cpp
+++ Praktikum_3_4_Studierende/Sources_orig/fsm.cpp
@@ -10,199 +10,329 @@
 
 #include "fsm.h"
 
-FSM::FSM(FestoProcessAccess *process, Plugin* plugin = 0) {
-    this->process = process;
-    currentState = Start;
-    this->plugin = plugin;
+#define MEASURE0 3770
+#define MEASURE1 3360
+#define MEASURE2 3710
+#define MEASURE3 3410
+#define MEASURE4 3050
+#define DEVIATION 100
+
+FSM::FSM(FestoProcessAccess *process, Plugin* plugin = 0)
+{
+	this->process = process;
+	currentState = Start;
+	this->plugin = plugin;
+	hightState = HightMeasure0;
 }
 
-FSM::~FSM() {
-    delete plugin;
-    delete process;
+FSM::~FSM()
+{
+	delete plugin;
+	delete process;
 }
 
-void FSM::eval() {
-    process->updateInputs();
-    if (plugin != NULL) {
-        plugin->evalCycle();
-    }
-    evalEvents();
-    evalState();
-    process->applyOutput();
+void FSM::eval()
+{
+	process->updateInputs();
+	if (plugin != NULL)
+	{
+		plugin->evalCycle();
+	}
+	evalEvents();
+	evalState();
+	process->applyOutput();
 }
 
-void FSM::evalEvents() {
-    switch (currentState) {
-        case Start:
-            currentState = Standby;
-            break;
-        case Standby:
-            if (process->isButtonStartNegativeEdge()) {
-                currentState = Ready;
-            }
-            break;
-        case Ready:
-            if (process->isButtonStartNegativeEdge()) {
-                currentState = Standby;
-            }
-            if (process->isItemAtBeginning()&& !process->isItemAtEnd()) {
-                currentState = Transport;
-            }
-            if (process->isItemAtHightSensor() || process->isItemAtMetalDetector()) {
-                currentState = Error;
-            }
-            break;
-        case Transport:
-        	//Tue Höhenmessung!!!-------------------------------------------------------------------------------------------------------
-            if (process->isItemAtMetalDetector()) {
-                currentState = MetalDetection;
-            }
-            if (process->isItemAtEnd()) {
-                currentState = Error;
-            }
-            break;
-        case MetalDetection:
-            if (process->isMetalDetected()) {
-                currentState = Metalic;
-            } else {
-                currentState = NonMetalic;
-            }
-            break;
-        case NonMetalic:
-            if (process->isItemAtEnd()) {
-                currentState = EndReached;
-            }
-            if (process->isItemAtBeginning() || process->isItemAtHightSensor()) {
-                currentState = Error;
-            }
-            break;
-        case EndReached:
-            if (process->isButtonStartNegativeEdge()) {
-                currentState = Standby;
-            }
-            if (!(process->isItemAtEnd())) {
-                currentState = Ready;
-            }
-            if (process->isItemAtMetalDetector() || process->isItemAtHightSensor()) {
-                currentState = Error;
-            }
-            break;
-        case Metalic:
-            if (process->hasItemPassedSlide()) {
-                currentState = SlideReached;
-            }
-            if (process->isItemAtBeginning() || process->isItemAtHightSensor()) {
-                currentState = Error;
-            }
-            break;
-        case SlideReached:
-            if (process->isButtonStartNegativeEdge()) {
-                currentState = Standby;
-            }
-            if (process->isItemAtBeginning()) {
-                currentState = Transport;
-            }
-            if (process->isItemAtEnd() || process->isItemAtHightSensor()) {
-                currentState = Error;
-            }
-            break;
-        case Error:
-            if (process->isButtonStartNegativeEdge()) {
-                currentState = Standby;
-            }
-            break;
-        default:
-            currentState = Start;
-    }
+void FSM::evalEvents()
+{
+	switch (currentState) {
+	case Start:
+		currentState = Standby;
+		break;
+	case Standby:
+		if (process->isButtonStartNegativeEdge())
+		{
+			currentState = Ready;
+		}
+		break;
+	case Ready:
+		if (process->isButtonStartNegativeEdge())
+		{
+			currentState = Standby;
+		}
+		if (process->isItemAtBeginning() && !process->isItemAtEnd())
+		{
+			currentState = Transport;
+		}
+		if (process->isItemAtHightSensor() || process->isItemAtMetalDetector())
+		{
+			currentState = Error;
+		}
+		break;
+	case Transport:
+		//Tue Höhenmessung!!!-------------------------------------------------------------------------------------------------------
+		if (!checkHight())
+		{
+			currentState = WrongBlock;
+		}
+		if (process->isItemAtMetalDetector())
+		{
+			currentState = MetalDetection;
+		}
+		if (process->isItemAtEnd())
+		{
+			currentState = Error;
+		}
+		break;
+	case WrongBlock:
+		if (process->isItemAtBeginning())
+		{
+			currentState = BeginningReached;
+		}
+		break;
+	case BeginningReached:
+		if (!process->isItemAtBeginning())
+		{
+			currentState = Standby;
+		}
+		break;
+	case MetalDetection:
+		if (process->isMetalDetected())
+		{
+			currentState = Metalic;
+		} else
+		{
+			currentState = NonMetalic;
+		}
+		break;
+	case NonMetalic:
+		if (process->isItemAtEnd())
+		{
+			currentState = EndReached;
+		}
+		if (process->isItemAtBeginning() || process->isItemAtHightSensor())
+		{
+			currentState = Error;
+		}
+		break;
+	case EndReached:
+		if (process->isButtonStartNegativeEdge())
+		{
+			currentState = Standby;
+		}
+		if (!(process->isItemAtEnd()))
+		{
+			currentState = Ready;
+		}
+		if (process->isItemAtMetalDetector() || process->isItemAtHightSensor())
+		{
+			currentState = Error;
+		}
+		break;
+	case Metalic:
+		if (process->hasItemPassedSlide())
+		{
+			currentState = SlideReached;
+		}
+		if (process->isItemAtBeginning() || process->isItemAtHightSensor())
+		{
+			currentState = Error;
+		}
+		break;
+	case SlideReached:
+		if (process->isButtonStartNegativeEdge())
+		{
+			currentState = Standby;
+		}
+		if (process->isItemAtBeginning())
+		{
+			currentState = Transport;
+		}
+		if (process->isItemAtEnd() || process->isItemAtHightSensor())
+		{
+			currentState = Error;
+		}
+		break;
+	case Error:
+		if (process->isButtonStartNegativeEdge())
+		{
+			currentState = Standby;
+		}
+		break;
+	default:
+		currentState = Start;
+	}
 }
 
-void FSM::evalState() {
-    switch (currentState) {
-        case Start:
-        case Standby:
-            process->driveStop();
-            process->lightGreenOff();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDQ1Off();
-            process->turnLEDQ2Off();
-            process->turnLEDResetOff();
-            process->turnLEDStartOn();
-            break;
-        case Ready:
-            process->driveStop();
-            process->lightGreenOn();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDStartOn();
-            break;
-        case Transport:
-            process->driveRight();
-            process->lightGreenOn();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDStartOff();
-            break;
-        case MetalDetection:
-            process->driveStop();
-            process->lightGreenOn();
-            process->lightRedOff();
-            process->lightYellowOff();
-            break;
-        case NonMetalic:
-            process->driveRight();
-            process->lightGreenOn();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDStartOff();
-            process->openJunction();
-            break;
-        case EndReached:
-            process->driveStop();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDStartOff();
-            process->closeJunction();
-            process->turnLEDStartOn();
-            blinkGreen();
-            break;
-        case Metalic:
-            process->driveRight();
-            process->lightGreenOff();
-            process->lightRedOff();
-            process->lightYellowOn();
-            break;
-        case SlideReached:
-            process->driveStop();
-            process->lightGreenOn();
-            process->lightRedOff();
-            process->lightYellowOff();
-            process->turnLEDStartOn();
-            break;
-        case Error:
-            process->driveStop();
-            process->lightGreenOff();
-            process->lightYellowOff();
-            process->turnLEDStartOn();
-            process->closeJunction();
-            blinkRed();
-            break;
+void FSM::evalState()
+{
+	switch (currentState) {
+	case Start:
+	case Standby:
+		process->driveStop();
+		process->lightGreenOff();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDQ1Off();
+		process->turnLEDQ2Off();
+		process->turnLEDResetOff();
+		process->turnLEDStartOn();
+		break;
+	case Ready:
+		process->driveStop();
+		process->lightGreenOn();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDStartOn();
+		break;
+	case Transport:
+		process->driveRight();
+		process->lightGreenOn();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDStartOff();
+		break;
+	case WrongBlock:
+		process->driveLeft();
+		process->lightGreenOff();
+		blinkRed();
+		process->lightYellowOff();
+		break;
+	case BeginningReached:
+		process->driveStop();
+		process->lightGreenOff();
+		blinkRed();
+		process->lightYellowOff();
+		break;
+	case MetalDetection:
+		process->driveStop();
+		process->lightGreenOn();
+		process->lightRedOff();
+		process->lightYellowOff();
+		break;
+	case NonMetalic:
+		process->driveRight();
+		process->lightGreenOn();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDStartOff();
+		process->openJunction();
+		break;
+	case EndReached:
+		process->driveStop();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDStartOff();
+		process->closeJunction();
+		process->turnLEDStartOn();
+		blinkGreen();
+		break;
+	case Metalic:
+		process->driveRight();
+		process->lightGreenOff();
+		process->lightRedOff();
+		process->lightYellowOn();
+		break;
+	case SlideReached:
+		process->driveStop();
+		process->lightGreenOn();
+		process->lightRedOff();
+		process->lightYellowOff();
+		process->turnLEDStartOn();
+		break;
+	case Error:
+		process->driveStop();
+		process->lightGreenOff();
+		process->lightYellowOff();
+		process->turnLEDStartOn();
+		process->closeJunction();
+		blinkRed();
+		break;
 
-    }
+	}
 }
 
-void FSM::blinkRed() {
-    if (process->timeCounter1s() & 0x01) {
-        process->lightRedOn();
-    } else {
-        process->lightRedOff();
-    }
+void FSM::blinkRed()
+{
+	if (process->timeCounter1s() & 0x01)
+	{
+		process->lightRedOn();
+	} else
+	{
+		process->lightRedOff();
+	}
 }
 
-void FSM::blinkGreen() {
-    if (process->timeCounter1s() & 0x01) {
-        process->lightGreenOn();
-    } else {
-        process->lightGreenOff();
-    }
+void FSM::blinkGreen()
+{
+	if (process->timeCounter1s() & 0x01)
+	{
+		process->lightGreenOn();
+	} else
+	{
+		process->lightGreenOff();
+	}
+}
+
+bool FSM::checkHight()
+{
+	switch (hightState) {
+	case HightMeasure0:
+		if (!(process->getHight() < MEASURE1 - DEVIATION
+				|| process->getHight() > MEASURE1 + DEVIATION))
+		{
+			hightState = HightMeasure1;
+		} else if (process->getHight() < MEASURE0 - DEVIATION
+				|| process->getHight() > MEASURE0 + DEVIATION)
+		{
+			return false;
+		}
+		break;
+	case HightMeasure1:
+		if (!(process->getHight() < MEASURE2 - DEVIATION
+				|| process->getHight() > MEASURE2 + DEVIATION))
+		{
+			hightState = HightMeasure2;
+		} else if (process->getHight() < MEASURE1 - DEVIATION
+				|| process->getHight() > MEASURE1 + DEVIATION)
+		{
+			return false;
+		}
+		break;
+	case HightMeasure2:
+		if (!(process->getHight() < MEASURE3 - DEVIATION
+				|| process->getHight() > MEASURE3 + DEVIATION))
+		{
+			hightState = HightMeasure3;
+		} else if (process->getHight() < MEASURE2 - DEVIATION
+				|| process->getHight() > MEASURE2 + DEVIATION)
+		{
+			return false;
+		}
+		break;
+	case HightMeasure3:
+		if (!(process->getHight() < MEASURE4 - DEVIATION
+				|| process->getHight() > MEASURE4 + DEVIATION))
+		{
+			hightState = HightMeasure4;
+		} else if (process->getHight() < MEASURE3 - DEVIATION
+				|| process->getHight() > MEASURE3 + DEVIATION)
+		{
+			return false;
+		}
+		break;
+	case HightMeasure4:
+		if (!(process->getHight() < MEASURE0 - DEVIATION
+				|| process->getHight() > MEASURE0 + DEVIATION))
+		{
+			hightState = HightMeasure0;
+		} else if (process->getHight() < MEASURE4 - DEVIATION
+				|| process->getHight() > MEASURE4 + DEVIATION)
+		{
+			return false;
+		}
+		break;
+		break;
+	}
+	return true;
 }
--- Praktikum_3_4_Studierende/Sources_orig/fsm.h
+++ Praktikum_3_4_Studierende/Sources_orig/fsm.h
@@ -13,23 +13,45 @@
 #include "FestoProcessAccess.h"
 #include "plugin.h"
 
-enum fsm_states { Start, Standby, Ready, Transport, MetalDetection, NonMetalic, Metalic, SlideReached, Error, EndReached, HightMeasure1, HightMeasure2, HightMeasure3, HightMeasure4, HightMeasure5, WrongBlock};
+enum fsm_states
+{
+	Start,
+	Standby,
+	Ready,
+	Transport,
+	MetalDetection,
+	NonMetalic,
+	Metalic,
+	SlideReached,
+	Error,
+	EndReached,
+	HightMeasure0,
+	HightMeasure1,
+	HightMeasure2,
+	HightMeasure3,
+	HightMeasure4,
+	HightMeasure5,
+	WrongBlock,
+	BeginningReached
+};
 
-class FSM {
+class FSM
+{
 private:
-    fsm_states currentState;
-    FestoProcessAccess *process;
-    Plugin* plugin;
+	fsm_states currentState, hightState;
+	FestoProcessAccess *process;
+	Plugin* plugin;
 public:
-    FSM( FestoProcessAccess *process, Plugin* plugin);
-    ~FSM();
-    void eval();
+	FSM(FestoProcessAccess *process, Plugin* plugin);
+	~FSM();
+	void eval();
 private:
-    void evalEvents();
-    void evalState();
+	void evalEvents();
+	void evalState();
 private:
-    void blinkGreen();
-    void blinkRed();
+	void blinkGreen();
+	void blinkRed();
+	bool checkHight();
 };
 
 #endif	/* FSM_H */
